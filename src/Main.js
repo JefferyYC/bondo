1b49ee1fdae963ea62948b55665fc5c757da0c8c
Jeffery Chen <jefferyyc@gmail.com>
2020-07-15 23:03:40 -0700

    Incomplete main page

---
+++ src/Main.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div classNameName = "Main">
+                <div className = "Intro">
+                    <h1 id="introHeader"> 
+                        Bondo<br></br>World Leading Mentorship Platform
+                    </h1>
+                    <p id="introP">
+                        We are a platform dedicated to providing the best mentorship for you.
+                    </p>
+                    <p id="introP">
+                        You pay for your future!
+                    </p>
+                    <p id="introP">
+                        Don't hesitate. Sign up now!
+                    </p>
+                </div>
+                <div className = "Procedure">
+                    <h1 id="procedureHeader">
+                        Find a Mentor
+                    </h1>
+                    <p id="procedureP">
+                        Look through hundreds of experienced in various industries to find the one who suits you best.
+                    </p>
+                </div>
+                <div className = "Procedure">
+                    <h1 id="procedureHeader">
+                        Confirm Interest
+                    </h1>
+                    <p id="procedureP">
+                        Look through hundreds of experienced in various industries to find the one who suits you bestTell your mentor your need and interest. Confirm your mentorship with a refundable fee. .
+                    </p>
+                </div>
+                <div className = "Procedure">
+                    <h1 id="procedureHeader">
+                        Get “Exclusive” Mentorship
+                    </h1>
+                    <p id="procedureP">
+                        Get Exlusive Mentorship and make appointment with your private mentor!
+                    </p>
+                </div>
+                <div className = "MentorRow">
+                    <div className = "MentorCol">
+                        <h2>Column 1</h2>
+                    </div>
+                    <div className = "MentorCol">
+                        <h2>Column 2</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main
