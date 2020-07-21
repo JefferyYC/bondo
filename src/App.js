aa67c9497f93df30a0eb6a5957eb8c0dae2f6007
Jeffery Chen <jefferyyc@gmail.com>
2020-07-21 00:22:50 -0700

    added authentication frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
 import { Home } from './Home';
-import { About } from './About';
+import About from './About';
 import { NoMatch } from './NoMatch';
+import { AuthContext } from './context/auth';
+import PrivateRoute from './PrivateRoute';
+import Login from './Login'
+import Signup from './Signup'
 
 function App() {
+
+  const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
   return (
-    <Router>
-      <NavigationBar />
-      <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NoMatch} />
-      </Switch>   
-    </Router>
+    <AuthContext.Provider value={{authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <NavigationBar />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute path="/about" component={About} />
+            <Route component={NoMatch} />
+        </Switch>   
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
