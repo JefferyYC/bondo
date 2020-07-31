05cf3426a551687aca899601415ca4ec9b815f87
Yukai <luo_yukai_kevin@berkeley.edu>
2020-07-31 00:27:26 -0700

    added frontend token verification logic

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,19 +2,25 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../context/auth';
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
 function PrivateRoute({ component: Component, ...rest }) {
     const { authTokens } = useAuth();
+    dotenv.config();
+    try {
+        jwt.verify(authTokens, process.env.REACT_APP_TOKEN_SECRET);
+    } catch (err) {
+        return <Redirect to="/login" />
+    }
+    
+    
   
     return(
         <Route 
             {...rest} 
             render={props =>
-                authTokens=="test" ? (
                     <Component {...props} />
-                ) : (
-                    <Redirect to="/login" />
-                )
             }
         />
     );
