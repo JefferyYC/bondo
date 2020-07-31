4522aa3a30c820a657a311c9cd9b6be7cd27ccb0
Yukai <luo_yukai_kevin@berkeley.edu>
2020-07-30 18:12:29 -0700

    added sign up page

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -4,18 +4,77 @@ import logoImg from "../lyk.jpeg";
 import { Card, Logo, Form, Input, Button } from '../components/AuthForm';
 
 function Signup() {
-  return (
-    <Card>
-      <Logo src={logoImg} />
-      <Form>
-        <Input type="email" placeholder="email" />
-        <Input type="password" placeholder="password" />
-        <Input type="password" placeholder="password again" />
-        <Button>Sign Up</Button>
-      </Form>
-      <Link to="/login">Already have an account?</Link>
-    </Card>
-  );
+
+    const [isError, setIsError] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    function postSignUp() {
+      var postData = {
+        name: userName,
+        email: email,
+        password: password
+      };
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      axios.post("http://localhost:5000/api/user/signup", postData, config)
+      .then(result => {
+        if (result.status === 200) {
+          console.log("successful signed up")
+          return <Redirect to="/login" />
+        } else {
+          console.log("unsuccessful")
+          setIsError(true);
+        }
+      }).catch(e => {
+        console.log("error")
+        setIsError(true);
+      });
+    }
+
+    // if (isLoggedIn) {
+    //     return <Redirect to="/" />;
+    //   }
+
+    return (
+        <Card>
+            <Logo src={logoImg} />
+            <Form>
+                <Input
+                type="username"
+                value={userName}
+                onChange={e => {
+                    setUserName(e.target.value);
+                }}
+                placeholder="name"
+                />
+                <Input
+                type="email"
+                value={email}
+                onChange={e => {
+                    setEmail(e.target.value);
+                }}
+                placeholder="email"
+                />
+                <Input
+                type="password"
+                value={password}
+                onChange={e => {
+                    setPassword(e.target.value);
+                }}
+                placeholder="password"
+                />
+                <Button onClick={postSignUp}>Sign Up</Button>
+            </Form>
+            { isError &&<Error>The username or password provided were incorrect!</Error> }
+        </Card>
+    );
 }
 
 export default Signup;
