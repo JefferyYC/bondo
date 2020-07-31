8b83492adb2b88aa0f1f1c8e1182981bacc932f0
Yukai <luo_yukai_kevin@berkeley.edu>
2020-07-30 23:09:41 -0700

    added backend token verification logic

---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const verify = require('./verifyToken');
+
+router.get('/', verify, (req, res) => {
+    res.json({
+        posts: {
+            title: 'this is my first post. Testing', 
+            description: 'random data you shouldnot see'
+        }
+    });
+});
+
+
+
+module.exports = router;
