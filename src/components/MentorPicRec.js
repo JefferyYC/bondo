7fba9046f862c4193d1c3ecf059f6ad12e711495
Yukai <luo_yukai_kevin@berkeley.edu>
2020-08-04 00:00:51 -0700

    added mentor description component

---
+++ src/components/MentorPicRec.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../css/MentorProfile.css';
+
+
+function MentorPicRec(props) {
+    const style1 = {
+        height: props.height,
+        width: props.width
+    }
+    const style2 = {
+        backgroundImage: "url(" + props.url + ")",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'noRepeat'
+    }
+    
+    return (
+        <div className="profileblk" syyle={style1}>
+            <div className="profileblkpic" style={style2}></div>
+            <h3 id="description_mentor_name">Yukai Luo</h3>
+            <p id="description_mentor_content">
+            {props.data}
+            </p>
+            
+        </div>
+        
+    )
+}
+
+export default MentorPicRec;
--- src/routes/Platform.js
+++ src/routes/Platform.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Button } from "../components/AuthForm";
 import { useAuth } from "../context/auth";
+import lyk from '../lyk.jpeg';
+import MentorPicRec from '../components/MentorPicRec.js';
+import { Link } from "react-router-dom"
+import {testText} from '../testText.txt'
+
 
 function Platform() {
     const { setAuthTokens } = useAuth();
@@ -9,10 +14,16 @@ function Platform() {
       setAuthTokens("test");
     }
 
+    
+
     return (
         <div Platform>
             <h1> Platform</h1>
             <Button onClick={logOut}>Log out</Button>
+            <Link to="/mentorprofile">
+                <MentorPicRec data="This is a test" height="600px" width="500px" url= {lyk}/>
+            </Link>
+            
         </div>
     );
 }
