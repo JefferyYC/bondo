bba8156a9dc94d4ca4ed51ffcad6e4d5e0b474cd
Jeffery Chen <jefferyyc@gmail.com>
2020-08-04 00:24:35 -0700

    fix mentor block css

--- src/routes/Platform.js
+++ src/routes/Platform.js
@@ -21,7 +21,7 @@ function Platform() {
             <h1> Platform</h1>
             <Button onClick={logOut}>Log out</Button>
             <Link to="/mentorprofile">
-                <MentorPicRec data="This is a test" height="600px" width="500px" url= {lyk}/>
+                <MentorPicRec data="This is a test test te  is a test test te  is a test test te  is a test test te  is a test test te  is a test test te  is a test test te st te  is a test test te st te  is a test test te st te  is a test test te  " height="300px" width="250px" url= {lyk}/>
             </Link>
             
         </div>
