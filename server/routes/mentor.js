e0480722beb1151bcb147829c82b082b416d7e76
Jeffery Chen <jefferyyc@gmail.com>
2020-08-11 00:34:45 -0700

    remove null logs

--- server/routes/mentor.js
+++ server/routes/mentor.js
@@ -5,13 +5,16 @@ const User = require('../model/User');
 router.post('/', async (req, res) => {
     if (req.body.isEmpty) {
         const total = await User.countDocuments({}, function(err, count){
-            console.log(err);
+            if (err) {
+                console.log(err)
+            }
         });
         const postPerPage = req.body.postPerPage
         const currentPage = req.body.currentPage
-        console.log(postPerPage*(currentPage-1))
         const users = await User.find({}, {}, {sort: {date: -1}, skip: postPerPage*(currentPage-1), limit: postPerPage}, function(err, res) {
-            console.log(err);
+            if (err) {
+                console.log(err)
+            }
         });
         return res.send({total: total, users: users})
     } else {    
@@ -19,7 +22,9 @@ router.post('/', async (req, res) => {
         const users = await User.find(
             { $text: { $search: req.body.queryString } },   
             { score: { $meta: "textScore" } }, function(err, res) {
-                console.log(err)
+                if (err) {
+                    console.log(err)
+                }
             }).sort( { score: { $meta: "textScore" } })
         return res.send({remaining: 100, users: users})
     } 
